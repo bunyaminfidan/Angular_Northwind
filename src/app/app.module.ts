99b565a45c9d58c4d8632a4c99eb71297367bd3f
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2021-03-17 21:58:16 +0300

    pipe kullanımı ve ngModel ile forms veri al ver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http"; // api istek için http isteği için gerek
+import {FormsModule} from '@angular/forms' // ngModule kullanmak için. html de girilen texti alabilmek için.
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     CategoryComponent,
-    NaviComponent
+    NaviComponent,
+    VatAddedPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule //// api istek için http isteği için gerek
+    HttpClientModule, //// api istek için http isteği için gerek
+    FormsModule  // ngModule kullanmak için. html de girilen texti alabilmek için. benim uygulamama forms ile ilgili çalışmları desteğini ver
   ],
   providers: [],
   bootstrap: [AppComponent]
